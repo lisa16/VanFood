84405673e7ef5bdc16f3d09cc07774ed53395eaf
MarioCimet <mariocimet@gmail.com>
2014-10-22 12:05:02 -0700

    Added client and server Login classes

---
+++ VanFood/src/com/google/gwt/sample/vanfood/server/LoginServiceImpl.java
@@ -0,0 +1,15 @@
+package com.google.gwt.sample.vanfood.server;
+
+import com.google.gwt.sample.vanfood.client.LoginInfo;
+import com.google.gwt.sample.vanfood.client.LoginService;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class LoginServiceImpl extends RemoteServiceServlet implements LoginService{
+
+	@Override
+	public LoginInfo login(String requestUri) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
