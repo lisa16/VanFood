fd9788229b77c0c0158775114659a12bf580d7dc
IrisChu <iris.love.chocolate@gmail.com>
2014-10-29 12:02:56 -0700

    rename services

--- VanFood/src/com/google/gwt/sample/vanfood/client/MapServiceAsync.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.sample.vanfood.client;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface MapServiceAsync {
-	void getVendors(String[] symbols, AsyncCallback<Vendor[]> callback);
-}
