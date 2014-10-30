1e690804f6b4729d5d4128049ed72099a8ba2d3f
Lisa Wong <miss.lisa19@hotmail.com>
2014-10-30 11:38:12 -0700

    removed buttons, added dropdown menu, added isHighlighted method to vendor class

--- VanFood/src/com/google/gwt/sample/vanfood/client/VanFood.java
+++ VanFood/src/com/google/gwt/sample/vanfood/client/VanFood.java
@@ -13,6 +13,7 @@ import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -108,7 +109,7 @@ public class VanFood implements EntryPoint {
 		vendorPanel.add(mapPanel);
 		
 		// Add food type buttons
-		addButtons();
+		addDropDownMenu();
 
 	    // Assemble Main panel.
 	    mainPanel.add(signOutLink);
@@ -130,98 +131,28 @@ public class VanFood implements EntryPoint {
 	    vendorsFlexTable.setText(row, 1, vendor.getAddress());
 	    vendorsFlexTable.getCellFormatter().addStyleName(row, 1, "vendorColumn");
 	}
-
-	private void addButtons() {
-		Button pizzaButton = new Button("Pizza");
-		pizzaButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button ukranianButton = new Button("Ukranian");
-		ukranianButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button frenchButton = new Button("French");
-		frenchButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button seafoodButton = new Button("Seafood");
-		seafoodButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button chineseButton = new Button("Chinese");
-		chineseButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button caribbeanButton = new Button("Caribbean");
-		caribbeanButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button juiceButton = new Button("Juice and Smoothies");
-		juiceButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button comfortButton = new Button("Comfort Food");
-		comfortButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button hotdogButton = new Button("Hot Dogs");
-		hotdogButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button breakfastButton = new Button("Breakfast Foods");
-		breakfastButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button chestnutsButton = new Button("Chestnuts");
-		chestnutsButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button indianButton = new Button("Indian");
-		indianButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button mediterraneanButton = new Button("Mediterranean");
-		mediterraneanButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button fusionButton = new Button("Fusion");
-		fusionButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button coffeeButton = new Button("Coffee and Baked Goods");
-		coffeeButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button israeliButton = new Button("Israeli Kosher");
-		israeliButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button japaneseButton = new Button("Japanese");
-		japaneseButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button persianButton = new Button("Persian");
-		persianButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button sandwichesButton = new Button("Sandwiches");
-		sandwichesButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button mexicanButton = new Button("Mexican");
-		mexicanButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button porkButton = new Button("Pulled Pork");
-		porkButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button bbqButton = new Button("BBQ");
-		bbqButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button mideasternButton = new Button("Middle Eastern");
-		mideasternButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button vegButton = new Button("Vegan and Vegetarian");
-		vegButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button crepesButton = new Button("Crepes");
-		crepesButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button greekButton = new Button("Greek");
-		greekButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button vietnameseButton = new Button("Vietnamese");
-		vietnameseButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button thaiButton = new Button("Thai");
-		thaiButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button elsalvadorianButton = new Button("El Salvadorian");
-		elsalvadorianButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button saladButton = new Button("Salads");
-		saladButton.getElement().setClassName("btn btn-xs btn-primary");
-		Button westButton = new Button("West Coast Fare");
-		westButton.getElement().setClassName("btn btn-xs btn-primary");
-		
-		
-		buttonsPanel.add(pizzaButton);
-		buttonsPanel.add(ukranianButton);
-		buttonsPanel.add(frenchButton);
-		buttonsPanel.add(seafoodButton);
-		buttonsPanel.add(chineseButton);
-		buttonsPanel.add(caribbeanButton);
-		buttonsPanel.add(comfortButton);
-		buttonsPanel.add(breakfastButton);
-		buttonsPanel.add(chestnutsButton);
-		buttonsPanel.add(mediterraneanButton);
-		buttonsPanel.add(israeliButton);
-		buttonsPanel.add(japaneseButton);
-		buttonsPanel.add(persianButton);
-		buttonsPanel.add(sandwichesButton);
-		buttonsPanel.add(mexicanButton);
-		buttonsPanel.add(porkButton);
-		buttonsPanel.add(bbqButton);
-		buttonsPanel.add(mideasternButton);
-		buttonsPanel.add(vegButton);
-		buttonsPanel.add(crepesButton);
-		buttonsPanel.add(greekButton);
-		buttonsPanel.add(vietnameseButton);
-		buttonsPanel.add(thaiButton);
-		buttonsPanel.add(elsalvadorianButton);
-		buttonsPanel.add(saladButton);	
-		buttonsPanel.add(westButton);
+	
+	/**
+	 * Dropdown menu for food descriptions
+	 */
+	public void addDropDownMenu(){
+		ListBox lb = new ListBox();
+		for(Vendor v: vendors){
+			if(!isInMenu(lb, v.getFoodtype())){
+			lb.addItem(v.getFoodtype());
+			}
+		}
+		lb.setVisibleItemCount(1);
+		buttonsPanel.add(lb);
+	}
+	
+	public boolean isInMenu(ListBox lb, String foodType){
+		for(int i=0; i<lb.getItemCount(); i++){
+			if(lb.getItemText(i).equals(foodType)){
+				return true;				
+			}
+		}
+		return false;		
 	}
 	
 	private void handleError(Throwable error) {
